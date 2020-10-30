205325d700c67cd6406b8ed2508a2ed2b012ba3b
DonovanStark <drs474@nau.edu>
2020-10-29 20:34:41 -0700

    Almost finished code

--- lab6.py
+++ lab6.py
@@ -1,17 +1,49 @@
 # Donovan Stark(drs474@nau.edu) and James Zanetti(jaz239@nau.edu)
-
+import turtle
 
 def getdata():
-    pass
+    data = []
+    star_data = open('stars.txt')
+    star_data = star_data
+    for line in star_data:
+        line = line.rstrip('\n')
+        data += [line]
+    star_data.close()
     return data
-def convertcoord(lctn):
-    pass
-    return lctn
-def convertsize(sze):
-    pass
-    return sze
-def drawstar(lctn,sze)
-    pass
+    
+
+def cnvrtdata(point):
+    x = float(point[0])
+    y = float(point[1])
+    x = x*250
+    y = y*250
+    m = (10/(float(point[4])+2)) 
+    if len(point) !=6:
+        n = point[6:]
+        cur = [x,y,m,n]
+        return cur
+    else:
+        cur = [x,y,m]
+        return cur
+def drawstar(loops,stars):
+    for i in range(loops):
+        turtle.penup()
+        turtle.goto(stars[i][0],stars[i][1])
+        turtle.seth(0)
+        turtle.pendown()
+        for _ in range(4):
+            turtle.forward(stars[3])
+            turtle.right(90)
 def connectstars():
     pass
 
+def main():
+    data = getdata()
+    loops = len(data)
+    stars = []
+    for i in range(len(data)):
+        point = data[i].split()
+        stars += [cnvrtdata(point)]
+    drawstar(loops,stars)
+
+main()
