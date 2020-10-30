a0cad90854135295ed211c7d8a498fb6d7ef223f
DonovanStark <drs474@nau.edu>
2020-10-29 18:15:36 -0700

    Basic outline of needed functions

--- lab6.py
+++ lab6.py
@@ -1,1 +1,17 @@
 # Donovan Stark(drs474@nau.edu) and James Zanetti(jaz239@nau.edu)
+
+
+def getdata():
+    pass
+    return data
+def convertcoord(lctn):
+    pass
+    return lctn
+def convertsize(sze):
+    pass
+    return sze
+def drawstar(lctn,sze)
+    pass
+def connectstars():
+    pass
+
